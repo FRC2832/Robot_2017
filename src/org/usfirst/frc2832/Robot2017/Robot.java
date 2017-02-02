a33ce73dde2daae4faab6a4f1aaf29e439e78d4c
johnd <johnd@John-Laptop>
2017-02-01 19:26:06 -0500

    2 Cameras displaying on the smartdashboard, Displaying encoder values to the smartDashboard

--- src/org/usfirst/frc2832/Robot2017/Robot.java
+++ src/org/usfirst/frc2832/Robot2017/Robot.java
@@ -74,6 +74,24 @@ public class Robot extends IterativeRobot {
 	            outputStream.putFrame(output);
 	        }
         }).start();
+    	
+    	new Thread(() -> {
+	    	
+	        
+	        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+	        camera.setResolution(720, 480);
+	        CvSink cvSink = CameraServer.getInstance().getVideo();
+	        CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+	        
+	        Mat source = new Mat();
+	        Mat output = new Mat();
+	        
+	        while(true) {
+	            cvSink.grabFrame(source);
+	            Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+	            outputStream.putFrame(output);
+	        }
+        }).start();
         
 
         driveTrain = new DriveTrain();
