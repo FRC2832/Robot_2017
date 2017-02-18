5d8d9612073d1bf2edc59b3d24a0db1d1bd2e229
LogicMaster63 <justinmarentette11@gmail.com>
2017-02-18 10:52:48 -0500

    Tried to fix brake mode when disabled and turned of compressor while climbing

--- src/org/usfirst/frc2832/Robot2017/commands/Climb.java
+++ src/org/usfirst/frc2832/Robot2017/commands/Climb.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc2832.Robot2017.commands;
 
 import org.usfirst.frc2832.Robot2017.Robot;
+import org.usfirst.frc2832.Robot2017.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -17,6 +18,7 @@ public class Climb extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	RobotMap.compressor.stop();
     	Robot.isClimbing = true;
     	if(Robot.lTrigger > 0.1) {
     		fixedSpeed = false;
@@ -47,11 +49,13 @@ public class Climb extends Command {
     // Called once after isFinished returns true
     protected void end() {
     	Robot.isClimbing = false;
+    	RobotMap.compressor.start();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	Robot.isClimbing = false;
+    	RobotMap.compressor.start();
     }
 }
