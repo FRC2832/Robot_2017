eab04c9bfe5f2d362cd19d8d0098ff4f306bb09d
Peter Zobel <peterz@mi.rr.com>
2017-03-28 19:10:41 -0400

    Updates to work with RotateWithPID

--- src/org/usfirst/frc2832/Robot2017/subsystems/NavX.java
+++ src/org/usfirst/frc2832/Robot2017/subsystems/NavX.java
@@ -3,11 +3,11 @@ package org.usfirst.frc2832.Robot2017.subsystems;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.command.Subsystem;
-//import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SerialPort;
 import com.kauailabs.navx.frc.AHRS;
 
 public class NavX extends Subsystem implements PIDOutput {
+    public static AHRS ahrs;
 	public static PIDController turnController;
 	public static final double kToleranceDegrees = 2;
 	public static double rotateToAngleRate;
@@ -15,12 +15,25 @@ public class NavX extends Subsystem implements PIDOutput {
 	static final double kI = 0.00;
 	static final double kD = 0.00;
 	static final double kF = 0.00;
-	public static AHRS ahrs;
 
+	public NavX() {
+		/***********************************************************************
+		 * navX-MXP: 
+		 * - Communication via RoboRIO MXP (SPI, I2C, TTL UART) and USB.            
+		 * - See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+		 * 
+		 * navX-Micro:
+		 * - Communication via I2C (RoboRIO MXP or Onboard) and USB.
+		 * - See http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+		 * 
+		 * Multiple navX-model devices on a single robot are supported.
+		 ************************************************************************/
+
+		ahrs = new AHRS(SerialPort.Port.kUSB);
+	}
+	
 	@Override
 	protected void initDefaultCommand() {
-		//ahrs = new AHRS(SPI.Port.kMXP); 
-		ahrs = new AHRS(SerialPort.Port.kUSB);
 		turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
 		turnController.setInputRange(-180.0f,  180.0f);
 		turnController.setOutputRange(-1.0, 1.0);
@@ -36,6 +49,10 @@ public class NavX extends Subsystem implements PIDOutput {
 	
 	public static double getHeading() {
         return ahrs.getAngle();
+    }	
+	
+	public static double getYaw() {
+        return ahrs.getYaw();
     }
 	
 }
