709b86e9bd5a6201f48b4b970c13f88c3efc4c5c
Patrick Hurley <phurley@gmail.com>
2017-04-04 07:37:40 -0400

    Add navx closed loop rotate, seeded by pixy

---
+++ src/org/usfirst/frc2832/Robot2017/autonCommands/WaitForCamera.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc2832.Robot2017.autonCommands;
+
+import org.usfirst.frc2832.Robot2017.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class WaitForCamera extends Command {
+    private double started;
+
+    @Override
+    protected void initialize() {
+        // max wait time (seconds)
+        setTimeout(2);
+        started = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return isTimedOut() || (Robot.lastPixyRead >= started);
+    }
+}
